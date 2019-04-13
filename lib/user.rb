f16c6b02301c1fad61d31e6fb3ce65e449c189c4
David Stewart <davidstewart@gmail.com>
2019-04-13 18:57:46 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,7 @@
 class User
 
-attr_reader :first_name, :last_name
+attr_accessor :first_name, :last_name
 
-  def initialize(fname,lname)
-    @first_name = fname
-    @last_name = lname
-  end
+  
   
 end
